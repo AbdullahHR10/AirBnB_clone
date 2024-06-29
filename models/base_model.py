f7a094bdff0b59cbe1d083ca0b9d2d606cd1270b
AbdullahHR10 <AbdullahH.Ragab10>
2024-06-29 04:10:58 -0700

    Add args and kwargs conditions

--- models/base_model.py
+++ models/base_model.py
@@ -17,30 +17,41 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """
         Initializes MyClass with a unique ID, creation, and update timestamps.
-        
+
         Args:
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs:
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.datetime.strptime
+                            (kwargs[key], '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
         Returns a string representation of the instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime.
+        Updates the public instance attribute updated_at:
+        With the current datetime.
         """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        Returns a dictionary:
+        Containing all keys/values of __dict__ of the instance.
         """
         instance_dict = self.__dict__.copy()
         instance_dict['created_at'] = self.created_at.isoformat()
