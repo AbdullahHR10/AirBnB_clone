cd6aeb077283397b594b7c09ab37efa7642daf4f
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-01 01:34:20 -0700

    Add do_update

--- console.py
+++ console.py
@@ -89,6 +89,36 @@ class HBNBCommand(cmd.Cmd):
                 if key.split('.')[0] == args[0]:
                     print(str(value))
 
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id
+        by adding or updating attribute"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            objs = storage.all()
+            if objs is None:
+                print("** no instance found **")
+            else:
+                obj = objs[key]
+                attr_name = args[2]
+                attr_value = args[3]
+                try:
+                    attr_value = eval(attr_value)
+                except Exception:
+                    pass
+                setattr(obj, attr_name, attr_value)
+                obj.save
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
