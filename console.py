e4dbb823445311b449e8895819c730abcdcbf0fe
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-01 00:13:13 -0700

    Add do_create

--- console.py
+++ console.py
@@ -7,22 +7,31 @@ import models
 class HBNBCommand(cmd.Cmd):
     """Console class"""
     prompt = '(hbnb) '
-    def do_quit(self, line):
+
+    def do_quit(self, arg):
         """Exits the program"""
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """Exits the program"""
         return True
 
-    def do_help(self, line):
+    def do_help(self, arg):
         """Provides help information about available commands"""
-        cmd.Cmd.do_help(self, line)
+        cmd.Cmd.do_help(self, arg)
 
     def emptyline(self):
         """Does nothing when getting an empty line input"""
         pass
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)"""
+        if not arg:
+            print("** class name missing **")
+        class_name = arg.split()[0]
+        if class_name not in storage.classes:
+        print("** class doesn't exist **")
+        return
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
