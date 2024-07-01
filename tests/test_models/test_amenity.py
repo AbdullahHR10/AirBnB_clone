f050976d7a64a88e6f3ea2b8b6e4fe848bb312cf
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-01 05:38:39 -0700

    Add import Amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for testing Amenity class"""
+import models
+
+
+class TestUser(unittest.TestCase):
+    """Tests the User class"""
+
+    def test_user_initialization(self):
