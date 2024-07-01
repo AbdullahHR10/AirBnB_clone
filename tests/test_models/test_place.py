26ec7ab7f074fe449f8de58a6196f3678628a6cf
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-01 05:49:27 -0700

    Add Place test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -4,5 +4,18 @@ from models.Place import Place
 
 
 class TestPlace(unittest.TestCase):
-    """Tests Place"""
-
+    """Tests the Place class"""
+    def test_state_initialization(self):
+        """Test initialization of Place class"""
+        place_instance = Place()
+        self.assertEqual(place_instance.city_id, "")
+        self.assertEqual(place_instance.user_id, "")
+        self.assertEqual(place_instance.name, "")
+        self.assertEqual(place_instance.description, "")
+        self.assertEqual(place_instance.number_rooms, 0)
+        self.assertEqual(place_instance.number_bathrooms, 0)
+        self.assertEqual(place_instance.max_guest, 0)
+        self.assertEqual(place_instance.price_by_night, 0)
+        self.assertAlmostEqual(place_instance.latitude, 0.0)
+        self.assertAlmostEqual(place_instance.longitude, 0.0)
+        self.assertEqual(place_instance.amenity_ids, [])
