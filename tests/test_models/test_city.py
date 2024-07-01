c710ed51bd2f9aaf694b7a45b9ff09615b15087a
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-01 05:43:29 -0700

    Fix documentation

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Module for testing City class using unittests"""
-import models
+from models.city import City
 
 
 class TestCity(unittest.TestCase):
     """Tests the City class"""
-
     def test_city_initialization(self):
-        """Tests City"""
-       city_instance = City()
-       self.assertEqual(state_instance.state_id, "")
-       self.assertEqual(state_instance.name, "")
+        """Test initialization of State class"""
+        city_instance = City()
+        self.assertEqual(state_instance.state_id, "")
+        self.assertEqual(state_instance.name, "")
