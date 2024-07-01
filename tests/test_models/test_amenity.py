ffdbe1a604caf9b8d9de104a6bc5026c0d0bbc5a
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-01 05:45:39 -0700

    Add Amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Module for testing Amenity class"""
-import models
-
+from models.amenity import Amenity
 
 class TestUser(unittest.TestCase):
     """Tests the User class"""
 
     def test_user_initialization(self):
+        """Test initialization of Amenity class"""
+        amenity_instance = Amenity()
+        self.assertEqual(state_instance.name, "")
---
+++ tests/test_models/test_place.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for testing State class using unittests"""
+from models.Place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """Tests Place"""
+
