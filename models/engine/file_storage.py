658a014b916ddea44589dc8702088c2e859e0ed0
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-10 09:33:12 -0700

    Fix issues

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 
 class FileStorage:
     """Serializes instances to a JSON file and
-    deserializes JSON file to instance"""
+    deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
 
@@ -15,17 +15,16 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        """Sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        obj_dict = {}
-        for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as file:
-            json.dump(self.__objects, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
@@ -34,10 +33,10 @@ class FileStorage:
                 obj_dict = json.load(file)
                 for key, value in obj_dict.items():
                     class_name = key.split('.')[0]
-                    if class_name == 'BaseModel':
-                        obj = BaseModel(**value)
-                    else:
-                        continue
-                    self.__objects[key] = obj
+                    cls = globals().get(class_name)
+                    if cls:
+                        obj = cls(**value)
+                        self.__objects[key] = obj
         except FileNotFoundError:
             pass
+
