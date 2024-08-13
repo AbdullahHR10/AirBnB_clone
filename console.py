586f6e7c194582755627cd24cfdf4694de75a17a
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-12 23:36:13 -0700

    Remove error check

--- console.py
+++ console.py
@@ -45,9 +45,7 @@ class HBNBCommand(cmd.Cmd):
                 class_name, rest = line.split('.', 1)
                 method_name, args = rest.split('(', 1)
                 args = args.strip(')')
-
-            except ValueError as e:
-                print(f"Error: {e}")  # Debug print
+            except ValueError:
                 print(f"*** Unknown syntax: {line}")
                 return
             class_name, method = line.split('.')
