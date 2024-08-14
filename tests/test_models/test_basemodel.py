0c583f0fb7b7dd6f95498c1e0a14561f945faf52
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-14 00:49:43 -0700

    Add BaseModel tests

---
+++ tests/test_models/test_basemodel.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module that contains unittests for BaseModel class"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Unittests that tests BaseModel class"""
+
+    def setUp(self):
+        """Sets up environment for tests"""
+        self.instance = BaseModel()
+        kwargs = {
+            "id": "123",
+            "created_at": "2005-04-03T05:30:00.000000",
+            "updated_at": "2005-04-03T05:30:00.000000",
+        }
+        self.kwargs_instance = BaseModel(**kwargs)
+
+    def test_kwargs_initialization(self):
+        """Tests initialization with kwargs"""
+        self.assertEqual(self.kwargs_instance.id, "123")
+        self.assertEqual(self.kwargs_instance.created_at.isoformat(),
+                         "2005-04-03T05:30:00")
+        self.assertEqual(self.kwargs_instance.updated_at.isoformat(),
+                         "2005-04-03T05:30:00")
+
+    def test_no_kwargs_initialization(self):
+        """Tests initialization without kwargs"""
+        self.assertIsNotNone(self.instance)
+        self.assertIsInstance(self.instance.created_at, datetime)
+        self.assertIsInstance(self.instance.updated_at, datetime)
+
+    def test_attributes(self):
+        """Tests attributes"""
+        self.assertIsInstance(self.instance.id, str)
+        self.assertIsInstance(self.instance.created_at, datetime)
+        self.assertIsInstance(self.instance.updated_at, datetime)
+
+    def test_dict(self):
+        """Tests to_dict method"""
+        instance_dict = self.instance.to_dict()
+        self.assertIsInstance(instance_dict, dict)
+        self.assertEqual(instance_dict['__class__'], 'BaseModel')
+        self.assertIn('id', instance_dict)
+        self.assertIn('created_at', instance_dict)
+        self.assertIn('updated_at', instance_dict)        
+
+    def test_str(self):
+        """Tests __str__ method"""
+        string_rep = str(self.instance)
+        expected = f"[BaseModel] ({self.instance.id}) {self.instance.__dict__}"
+        self.assertEqual(string_rep, expected)        
+
+
+if __name__ == "__main__":
+    unittest.main()
