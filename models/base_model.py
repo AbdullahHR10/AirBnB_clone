c517c1a2d4a7047c7ff5efc50f1c5fcad177f373
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-14 00:39:57 -0700

    Fix storage

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel():
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Return a string representation of BaseModel instance"""
@@ -28,7 +28,7 @@ class BaseModel():
         """Updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
