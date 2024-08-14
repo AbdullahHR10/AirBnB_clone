12ac668308b7d832607d6256e528745a88e68fb6
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-13 23:13:12 -0700

    Remove microseconds from kwargs test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -25,9 +25,9 @@ class TestCity(unittest.TestCase):
         """Tests initialization with kwargs"""
         self.assertEqual(self.kwargs_instance.id, "123")
         self.assertEqual(self.kwargs_instance.created_at.isoformat(),
-                         "2005-04-03T05:30:00.000000")
+                         "2005-04-03T05:30:00")
         self.assertEqual(self.kwargs_instance.updated_at.isoformat(),
-                         "2005-04-03T05:30:00.000000")
+                         "2005-04-03T05:30:00")
         self.assertEqual(self.kwargs_instance.state_id, "456")
         self.assertEqual(self.kwargs_instance.name, "Cairo")
 
