40fd0d2173f4ecb1d7962305ba7757bff2484cdf
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-01-25 07:00:29 +0200

    Fix reload method.
    - Added classes dictionary to include classes
    - Removed 2 if conditions for 'created_at' and 'updated_at'
    - Changed the way the method fetches the class obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,7 +14,7 @@ class FileStorage():
     """ Serializes instances to a JSON file
     and deserializes JSON file to instances. """
     __file_path = "file.json"
-    __objects = {}  # Will store all objects by <class name>.id
+    __objects = {}  # Will store all objects by <class name>.id.
 
     def all(self):
         """ Returns the dictionary __objects. """
@@ -41,19 +41,17 @@ class FileStorage():
         (only if the JSON file (__file_path) exists otherwise, do nothing.
         If the file doesn't exist, no exception is raised).
         """
+        # Import all classes.
+        from models.base_model import BaseModel
+        # Add all classes to a dictionary.
+        classes = {'BaseModel': BaseModel}
         try:
             with open(self.__file_path, "r") as file:
                 data = json.load(file)
             for value in data.values():
-                if 'created_at' in value:
-                    value['created_at'] = datetime.fromisoformat(
-                        value['created_at'])
-                if 'updated_at' in value:
-                    value['updated_at'] = datetime.fromisoformat(
-                        value['updated_at'])
                 class_name = value['__class__']
-                # Get the class object dynamically
-                class_obj = globals()[class_name]
+                # Get the class object from the classes dictionary.
+                class_obj = classes.get(class_name)
                 obj = class_obj(**value)
                 self.new(obj)
 
